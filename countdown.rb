6d4641f66678ef8487bf0a5bc952dcabb45fe346
Kyle Talmage <ktalmage85@gmail.com>
2020-02-11 15:09:20 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
-#write your code here
 
-def countdown
+#write your code here
+def countdown_with_sleep(int)
+  sleep(int)
 end
+
+
+
+
+
+def countdown(secs)
+  number = secs
+  while number > 0
+   puts "#{number} SECOND(S)!"
+    number -= 1
+    countdown_with_sleep(1)
+    end
+  if number == 0
+  return "HAPPY NEW YEAR!"
+  end
+end
+
+
+
+
